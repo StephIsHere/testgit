a7f837f8237f08f18be49dbf4bd55a3591ff257c
Steph <stevenfebrian401@gmail.com>
2024-10-02 07:57:46 +0700

    update ver1

--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-	arr := make([]int, 3)
-	for i := 0; i < len(arr); i++ {
-		arr[i] = rand.Int()
-		fmt.Println(arr[i])
+	for i := 0; i < 1000; i++ {
+		fmt.Println(rand.NormFloat64() * 100)
 	}
-
 }
